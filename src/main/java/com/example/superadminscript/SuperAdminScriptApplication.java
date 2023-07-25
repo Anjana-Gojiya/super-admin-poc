496f995a973b4869650519d3cee3583044612a2c
anjana <anjana.gojiya@globalvoxinc.com>
2023-07-25 12:15:09 +0530

    init

---
+++ src/main/java/com/example/superadminscript/SuperAdminScriptApplication.java
@@ -0,0 +1,13 @@
+package com.example.superadminscript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperAdminScriptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperAdminScriptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/superadminscript/SuperAdminScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superadminscript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperAdminScriptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
